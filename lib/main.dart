a70ea5c5116108c7c5921e191aa84b0ec5fc541b
Artur Antin <antin.artur@gmail.com>
2021-06-09 12:38:04 +0200

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _navIndex = 1;
-  bool _isLoading = false;
+  int _navIndex = 0;
   List<String> cats = [
     "assets/cat0.jpg",
     "assets/cat1.jpg",
@@ -52,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<List<Person>> loadPeople() async {
     try {
       final response = await http.get(
-          Uri.parse("https://randomuser.me/api/?results=50&nat=de&noinfo"));
+          Uri.parse("https://randomuser.me/api/?results=1000&nat=de&noinfo"));
       if (response.statusCode == 200) {
         List<Person> res = [];
         for (var person in jsonDecode(response.body)['results'])
@@ -64,17 +63,12 @@ class _MyHomePageState extends State<MyHomePage> {
       }
     } catch (e) {
       throw Exception('Failed to load people - ${e.toString()}');
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     if (_navIndex == 1) cats.shuffle();
-    // imageCache.clear();
 
     return Scaffold(
       appBar: AppBar(
@@ -82,114 +76,15 @@ class _MyHomePageState extends State<MyHomePage> {
           _navIndex == 0 ? "Internet Daten" : "Lokale Daten",
         ),
       ),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : _navIndex == 0
-              ? FutureBuilder(
-                  future: loadPeople(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData)
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    return Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              icon: Icon(Icons.refresh),
-                              onPressed: () {
-                                setState(() {
-                                  _isLoading = true;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                        Expanded(
-                          child: SingleChildScrollView(
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Column(
-                                children: snapshot.data
-                                    .map<Widget>(
-                                      (Person item) => Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.all(4.0),
-                                            child: Text(
-                                              "${item.firstName} ${item.lastName}",
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 22,
-                                              ),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.all(4.0),
-                                            child: Text(
-                                              item.userName,
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.w300,
-                                                fontSize: 18,
-                                              ),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.all(4.0),
-                                            child: Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.spaceAround,
-                                              children: [
-                                                Padding(
-                                                  padding:
-                                                      const EdgeInsets.all(4.0),
-                                                  child: Image.network(
-                                                    item.pictureUrl,
-                                                    height: 80,
-                                                    width: 100,
-                                                  ),
-                                                ),
-                                                Column(
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment
-                                                          .spaceAround,
-                                                  children: [
-                                                    Text(
-                                                      "${item.streetName} ${item.streetNumber}",
-                                                    ),
-                                                    Text(
-                                                      "${item.postCode}, ${item.city}",
-                                                    )
-                                                  ],
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            height: 15,
-                                          )
-                                        ],
-                                      ),
-                                    )
-                                    .toList(),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                )
-              : Column(
+      body: _navIndex == 0
+          ? FutureBuilder(
+              future: loadPeople(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData)
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                return Column(
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
@@ -203,23 +98,131 @@ class _MyHomePageState extends State<MyHomePage> {
                       ],
                     ),
                     Expanded(
-                      child: GridView(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            children: snapshot.data
+                                .map<Widget>(
+                                  (Person item) => Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(4.0),
+                                        child: Text(
+                                          "${item.firstName} ${item.lastName}",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 22,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(4.0),
+                                        child: Text(
+                                          item.userName,
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w300,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(4.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(4.0),
+                                              child: Image.network(
+                                                item.pictureUrl,
+                                                height: 80,
+                                                width: 100,
+                                                frameBuilder: (context,
+                                                    child,
+                                                    frame,
+                                                    wasSynchronouslyLoaded) {
+                                                  if (wasSynchronouslyLoaded ??
+                                                      false) {
+                                                    return child;
+                                                  }
+                                                  return AnimatedOpacity(
+                                                    child: child,
+                                                    opacity:
+                                                        frame == null ? 0 : 1,
+                                                    duration: const Duration(
+                                                        seconds: 1),
+                                                    curve: Curves.easeOut,
+                                                  );
+                                                },
+                                              ),
+                                            ),
+                                            Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceAround,
+                                              children: [
+                                                Text(
+                                                  "${item.streetName} ${item.streetNumber}",
+                                                ),
+                                                Text(
+                                                  "${item.postCode}, ${item.city}",
+                                                )
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 15,
+                                      )
+                                    ],
+                                  ),
+                                )
+                                .toList(),
+                          ),
                         ),
-                        children: cats
-                            .map<Widget>(
-                              (e) => Image.asset(
-                                e,
-                                height: 200,
-                                width: 150,
-                              ),
-                            )
-                            .toList(),
                       ),
                     ),
                   ],
+                );
+              },
+            )
+          : Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.refresh),
+                      onPressed: () {
+                        setState(() {});
+                      },
+                    ),
+                  ],
                 ),
+                Expanded(
+                  child: GridView(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    children: cats
+                        .map<Widget>(
+                          (e) => Image.asset(
+                            e,
+                            height: 200,
+                            width: 150,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ],
+            ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (value) {
           setState(() {
